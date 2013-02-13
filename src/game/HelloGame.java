4768021507f2c96d72958cb727e6a9719910acf9
Bradley Pollard <bradley.pollard12@ic.ac.uk>
2013-02-13 17:38:05 +0000

    Added a package of states to make the game go.

---
+++ src/game/HelloGame.java
@@ -0,0 +1,31 @@
+package game;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class HelloGame extends StateBasedGame {
+
+	public static final int MAINMENUSTATE          = 0;
+    public static final int GAMEPLAYSTATE          = 1;
+  
+    public HelloGame()
+    {
+        super("HelloGame");
+    }
+  
+    public static void main(String[] args) throws SlickException
+    {
+         AppGameContainer app = new AppGameContainer(new HelloGame());
+         app.setDisplayMode(800, 600, false);
+         app.setVSync(true);
+         app.start();
+    }
+  
+    @Override
+    public void initStatesList(GameContainer gameContainer) throws SlickException {
+        this.addState(new MainMenuState(MAINMENUSTATE));
+        this.addState(new GameplayState(GAMEPLAYSTATE));
+    }
+}
---
+++ src/game/MainMenuState.java
@@ -0,0 +1,44 @@
+package game;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class MainMenuState extends BasicGameState {
+
+	int stateID = -1;
+	  
+    MainMenuState( int stateID ) 
+    {
+       this.stateID = stateID;
+    }
+  
+    @Override
+    public int getID() {
+        return stateID;
+    }
+    
+    @Override
+	public void init(GameContainer gc, StateBasedGame sbg)
+			throws SlickException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render(GameContainer gc, StateBasedGame sbc, Graphics g)
+			throws SlickException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(GameContainer gc, StateBasedGame sbg, int delta)
+			throws SlickException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
