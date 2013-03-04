7dcc175639aa52368cd0cdcb516b645bd84e95cc
George Steed <gds12@imperial.ac.uk>
2013-03-04 19:00:36 +0000

    Forgot to add AI files :/

---
+++ src/entities/aistates/IdleEntity.java
@@ -0,0 +1,12 @@
+package entities.aistates;
+
+import entities.Entity;
+
+public class IdleEntity extends AINextMove {
+
+	@Override
+	public void updateEntity(Entity e) {
+		//do nothing
+	}
+
+}
---
+++ src/entities/aistates/RoamingEntity.java
@@ -0,0 +1,41 @@
+package entities.aistates;
+
+import map.TileProperty;
+import utils.MapLoader;
+import entities.Entity;
+
+public class RoamingEntity extends AINextMove {
+	
+	private static final float MARGIN = 0.25f;
+	
+	private float dx = 0.2f * ((int) (Math.random()*2) - 0.5f);
+
+	@Override
+	public void updateEntity(Entity e) {
+		if(dx > 0){
+			if("false".equals(MapLoader.getCurrentCell().getTile((int) e.getX() + e.getWidth(), (int) e.getY() + e.getHeight()).lookupProperty(TileProperty.BLOCKED))){
+				dx *= -1;
+			}else{
+				for(float y=e.getY() + MARGIN;y<e.getY() + e.getHeight();y+=0.5f){
+					if("true".equals(MapLoader.getCurrentCell().getTile((int) (e.getX() + 0.01f) + e.getWidth(), (int) y).lookupProperty(TileProperty.BLOCKED))){
+						dx *= -1;
+						break;
+					}
+				}
+			}
+		}else if(dx < 0){
+			if("false".equals(MapLoader.getCurrentCell().getTile((int) e.getX(), (int) e.getY() + e.getHeight()).lookupProperty(TileProperty.BLOCKED))){
+				dx *= -1;
+			}else{
+				for(float y=e.getY() + MARGIN;y<e.getY() + e.getHeight();y+=0.5f){
+					if("true".equals(MapLoader.getCurrentCell().getTile((int) (e.getX() - 0.01f), (int) y).lookupProperty(TileProperty.BLOCKED))){
+						dx *= -1;
+						break;
+					}
+				}
+			}
+		}
+		e.moveX(dx);
+	}
+
+}
