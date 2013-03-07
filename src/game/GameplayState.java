3834c45f9de4c7080121b902b9e6b0704e1725f6
Jeremiah Vocaturo <jeremiahvocaturo@gmail.com>
2013-03-07 21:16:05 +0000

    Nothing much

--- src/game/GameplayState.java
+++ src/game/GameplayState.java
@@ -40,10 +40,11 @@ public class GameplayState extends BasicGameState {
 		EnemyLoader.loadEnemies("data/enemydata.xml");
 		//map loading goes here. Needs a better home
 		//method needed to load all maps into their correct index in the array
-		MapLoader.setDimensions(3,1);
+		MapLoader.setDimensions(3,2);
 		MapLoader.loadMap("data/JezMap01.tmx", 0, 0);
 		MapLoader.loadMap("data/JezMap02.tmx", 1, 0);
 		MapLoader.loadMap("data/JezMap03.tmx", 2, 0);
+		MapLoader.loadMap("data/JezMap13.tmx", 2, 1);
 		//set initial map
 		player = new Player(2,2);
 		currentCell = MapLoader.setCurrentCell(player,0,0);
