c4286caaff5b8f3e4de47d131bb698bb2de42fd5
bp912 <bp912@ic.ac.uk>
2013-03-21 19:31:28 +0000

    Weapon fixes

--- src/items/Sword.java
+++ src/items/Sword.java
@@ -17,11 +17,11 @@ public class Sword extends Weapon {
     private boolean swung = false;
     private int counter = 0;
     private SoundGroup swingSound;
+    private int dir;
     
     public Sword(Rectangle hitbox, Image[] images, int damage) {
         super(hitbox, images, damage);
-        sprite.setPingPong(true); // TODO: implement this in the sword class rather than the weapon class
-                                  //  ^ FTFY
+        right.setPingPong(true); left.setPingPong(true);
 		// also TODO: make animation run through but then stop, ready to start at first frame again when called again
         // no idea what this actually means though...
         try {
@@ -41,11 +41,13 @@ public class Sword extends Weapon {
     public void attack(Player p) {
       //if sword isnt already swinging
         if (!swung) {
-            int dir = p.getDirection();
+            dir = p.getDirection();
             if (dir == 1) {
                 hitbox.setLocation(p.getX() + 1.1f, p.getY());
+                sprite = right;
             } else {
                 hitbox.setLocation(p.getX() - 1.0f, p.getY());
+                sprite = left;
             }
             swung = true;
             swingSound.playSingle();
@@ -57,14 +59,17 @@ public class Sword extends Weapon {
         counter += delta;
         // if counter hasn't played full sword animation
         // TODO: for some reason this turns out to be twice the animation length.  I made a quick fix by setting the sprite to ping pong mode, doubling the length
-        if (swung && counter <= defaultDuration * duration.length) {
-            int dir = p.getDirection(); //update sword location
+        if (swung && counter <= defaultDuration * duration.length * 2) {
+            dir = p.getDirection(); //update sword location
+            sprite.restart(); // restart sprite's internal counter
             if (dir == 1) {
                 hitbox.setLocation(p.getX() + 1.1f, p.getY());
+                sprite = right; //change for direction
             } else {
                 hitbox.setLocation(p.getX() - 1.0f, p.getY());
+                sprite = left;
             }
-            sprite.update(delta);
+            sprite.update(counter); //update sprite to correct frame in anim
             
             // do NOT pass in ALL enemies in cell, or this will be slow
             // find some way to pass only adjacent enemies to player.
@@ -82,12 +87,18 @@ public class Sword extends Weapon {
             //otherwise set swung to false and stop displaing sword
             swung = false;
             counter = 0;
+            left.restart();
+            right.restart(); //reset both anims
         }
     }
     
     @Override
     public void render() {
-        sprite.draw((int)((hitbox.getX()-2)*Config.getTileSize()), (int)((hitbox.getY()-2.5)*Config.getTileSize()), new Color(255,255,255));
+        int xOffset = 2 * dir;
+        if (dir == -1) {
+            xOffset /= 2;
+        }
+        sprite.draw((int)((hitbox.getX()-xOffset)*Config.getTileSize()), (int)((hitbox.getY()-2.5)*Config.getTileSize()), new Color(255,255,255));
     }
 
 }
