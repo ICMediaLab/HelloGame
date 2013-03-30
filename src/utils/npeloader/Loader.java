0baa84fc1182badcb5e46cdd517e647bbb50e66b
George Steed <gds12@imperial.ac.uk>
2013-03-30 19:34:27 +0000

    Added javadoc comments to Loader. Hopefully somebody understands it...

--- src/utils/npeloader/Loader.java
+++ src/utils/npeloader/Loader.java
@@ -15,6 +15,11 @@ import org.xml.sax.SAXException;
 import utils.XMLDocumentLoader;
 import entities.Entity;
 
+/**
+ * An abstract Entity loader for loading generic entities from xml files.
+ * @param <T> The class of entity to be loaded and the class containing the load and clear methods.<br />
+ * Must satisfy {@code <T extends Entity>}.
+ */
 public abstract class Loader<T extends Entity> {
 
 	private static final String DEFAULT_LOAD = "load";
@@ -22,11 +27,23 @@ public abstract class Loader<T extends Entity> {
 	private final Method loadMethod;
 	private final Method clearMethod;
 
+	/**
+	 * Create a new Loader object of specified loading and clearing methods.<br />
+	 * Note both methods must be public and static.
+	 * @param loadMethod
+	 * @param clearMethod
+	 */
 	protected Loader(Method loadMethod, Method clearMethod){
 		this.loadMethod = loadMethod;
 		this.clearMethod = clearMethod;
 	}
 	
+	/**
+	 * Create a new Loader object with methods found in the class specified of the names specified.
+	 * @param clazz The class containing the following two method members. Note both methods must be public and static.
+	 * @param loadMethodName A case-sensitive string representation of the method name to be invoked when loading an entity of class T.
+	 * @param clearMethodName A case-sensitive string representation of the method name to be invoked when clearing the set of loaded entities of class T.
+	 */
 	public Loader(Class<? extends T> clazz, String loadMethodName, String clearMethodName) {
 		Method load = null, clear = null;
 		try {
@@ -41,10 +58,19 @@ public abstract class Loader<T extends Entity> {
 		clearMethod = clear;
 	}
 	
+	/**
+	 * Creates a new Loader object based on the class specified and the default loading and clearing methods (i.e. {@code load} and {@code clearLoaded})
+	 * @param clazz The class containing the two method members. Note both methods must be public and static.
+	 */
 	public Loader(Class<? extends T> clazz) {
 		this(clazz,DEFAULT_LOAD,DEFAULT_CLEAR);
 	}
 
+	/**
+	 * Attempts to invoke a static method with no context, printing the stack trace if the method does not exist, cannot be accessed, or has incorrect arguments.
+	 * @param method The method to be invoked.
+	 * @param args The arguments with which to invoke the method. It is recommended that arrays are explicitly cast as (Object) when passed.
+	 */
 	private void printStackTraceInvoke(Method method, Object... args){
 		try {
 			method.invoke(null, args);
@@ -57,18 +83,40 @@ public abstract class Loader<T extends Entity> {
 		}
 	}
 
+	/**
+	 * Clears all entities currently loaded in the set.
+	 */
 	public void clearLoaded(){
 		printStackTraceInvoke(clearMethod);
 	}
 	
+	/**
+	 * Loads an entity to the set from the node specified.<br />
+	 * Note this method will only attempt to load a single entity.
+	 */
 	private void loadItem(Node item){
 		printStackTraceInvoke(loadMethod, item);
 	}
 	
+	/**
+	 * Loads a document asserting that the root node and it's child nodes have the specified name.
+	 * @param d The document from which to load entities.
+	 * @param root The root node value to be tested.
+	 * @param node The child node value to be tested.
+	 */
 	protected final void load(Document d, String root, String node){
 		loadOnly(d, root, node, null, null);
 	}
 	
+	/**
+	 * Loads a document asserting that the root node and it's child nodes have the specified name.<br />
+	 * Additionally, it will check the attribute {@code lookup} and only load the node if it is contained by {@code only}.
+	 * @param d The document from which to load entities.
+	 * @param root The root node value to be tested.
+	 * @param node The child node value to be tested.
+	 * @param lookup The attribute to be queried.
+	 * @param only The set of {@code lookup} values that should be loaded.
+	 */
 	protected final void loadOnly(Document d, String root, String node, String lookup, Collection<String> only){
 		if(d.getDocumentElement().getNodeName().equalsIgnoreCase(root)){
 			NodeList nList = d.getElementsByTagName(node);
@@ -83,11 +131,18 @@ public abstract class Loader<T extends Entity> {
 		}
 	}
 	
+	/**
+	 * Attempts to load all entities from xml documents corresponding to the paths specified.
+	 */
 	public final void load(String... paths){
 		load(null,paths);
 	}
-
-	private final void load(Collection<String> only, String... paths) {
+	
+	/**
+	 * Attempts to load all entities from xml documents corresponding to the paths specified.<br />
+	 * Will only load entities of lookup value contained by {@code only}. 
+	 */
+	public final void load(Collection<String> only, String... paths) {
 		try {
 			Document[] docs = XMLDocumentLoader.getXMLDocument(paths);
 			for(Document d : docs){
@@ -104,6 +159,10 @@ public abstract class Loader<T extends Entity> {
 		}
 	}
 
+	/**
+	 * Attempts to load entities from an xml document specified. Will only load those with lookup values contained by the set {@code only}.<br />
+	 * The details of the lookup attribute are left to the implementation.
+	 */
 	protected abstract void load(Document d, Collection<String> only);
 	
 }
