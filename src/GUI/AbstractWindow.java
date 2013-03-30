1d7edfb3370b31dd893127e112d5c8edcd19a5a7
George Steed <gds12@imperial.ac.uk>
2013-03-30 18:35:23 +0000

    Forgot to include AbstractWindow.

---
+++ src/GUI/AbstractWindow.java
@@ -0,0 +1,48 @@
+package GUI;
+
+import game.config.Config;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.state.StateBasedGame;
+
+import utils.Dimension;
+import utils.Position;
+
+
+public abstract class AbstractWindow {
+	
+	private static final Position NORMAL_TOPLEFT = new Position(Config.getScreenWidth()*0.25f,Config.getScreenHeight()*0.25f);
+	private static final Dimension NORMAL_DIMENSIONS = new Dimension(Config.getScreenWidth()*0.5f,Config.getScreenHeight()*0.5f);
+
+	private final Position topleft;
+	private final Dimension dimension;
+	
+	public AbstractWindow(){
+		this(NORMAL_TOPLEFT,NORMAL_DIMENSIONS);
+	}
+	
+	public AbstractWindow(float x, float y, float width, float height) {
+		this.topleft = new Position(x,y);
+		this.dimension = new Dimension(width, height);
+	}
+	public AbstractWindow(Position topleft, Dimension dimension) {
+		this.topleft = topleft;
+		this.dimension = dimension;
+	}
+
+	public float getX() {
+		return topleft.getX();
+	}
+	public float getY() {
+		return topleft.getY();
+	}
+	public float getWidth() {
+		return dimension.getWidth();
+	}
+	public float getHeight() {
+		return dimension.getHeight();
+	}
+	public abstract void render(Graphics gr);
+	public abstract void update(GameContainer gc, StateBasedGame sbg, float delta);
+}
