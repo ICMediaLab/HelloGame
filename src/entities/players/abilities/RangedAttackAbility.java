db88b19233df625d5720ee90306d049c9d7c781b
Paweł Palczyński <p.j.palczynski@gmail.com>
2013-04-02 15:49:22 +0200

    Added shooting arrow sound

--- src/entities/players/abilities/RangedAttackAbility.java
+++ src/entities/players/abilities/RangedAttackAbility.java
@@ -1,16 +1,27 @@
 package entities.players.abilities;
 
+import org.newdawn.slick.SlickException;
+
 import game.MouseCapture;
 import items.projectiles.Projectile;
+import sounds.SoundGroup;
 import utils.MapLoader;
 import utils.Position;
 import entities.players.Player;
 
 public class RangedAttackAbility extends PlayerAbility {
 	
+	private SoundGroup release;
+	
 	public RangedAttackAbility() {
 		this.name = "Ranged attack";
 		this.description = "Hold RMB and release to shoot";
+		try {
+			this.release = new SoundGroup("player/arrow/release");
+		} catch (SlickException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	@Override
@@ -20,6 +31,7 @@ public class RangedAttackAbility extends PlayerAbility {
 	    vec.translate(-p.getCentreX(), -p.getCentreY());
 	    pro = new Projectile(p.getCentreX(), p.getCentreY(), 10, vec.getAngle(), p.getRangedCounter());
 		MapLoader.getCurrentCell().addEntity(pro);
+		release.playSingle();
 	}
 
 }
