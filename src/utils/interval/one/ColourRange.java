d760774b13d4724252cacc62e5582c1d9d9832e8
George Steed <gds12@imperial.ac.uk>
2013-05-14 19:50:24 +0100

    Added minor javadoc clarification.

--- src/utils/interval/one/ColourRange.java
+++ src/utils/interval/one/ColourRange.java
@@ -2,10 +2,17 @@ package utils.interval.one;
 
 import org.newdawn.slick.Color;
 
+/**
+ * A class for holding a range of colours based on rgba components. Each component is held as a separate interval.
+ * The alpha channel will have a fixed value of 1f (opaque) if not specified in the constructor.
+ */
 public class ColourRange extends Range<Color>{
 	
 	private final Interval r,g,b,a;
 	
+	/**
+	 * Creates a new ColourRange object with specified intervals for the red, green, blue and alpha channels respectfully.
+	 */
 	public ColourRange(Interval r, Interval g, Interval b, Interval a) {
 		this.r = r;
 		this.g = g;
@@ -13,16 +20,39 @@ public class ColourRange extends Range<Color>{
 		this.a = a;
 	}
 	
+	/**
+	 * Creates a new ColourRange object with specified intervals for the red, green and blue channels respectfully.<br />
+	 * The alpha channel will be set to a fixed value of 1f (opaque).
+	 */
 	public ColourRange(Interval r, Interval g, Interval b) {
 		this(r,g,b,new FixedValue(1f));
 	}
 	
-	public ColourRange(float minr, float maxr, float ming, float maxg, float minb, float maxb, float mina, float maxa) {
-		this(new Interval(minr,maxr),new Interval(ming,maxg),new Interval(minb,maxb),new Interval(mina,maxa));
+	/**
+	 * Intermediate constructor for handling rgb components :)
+	 */
+	private ColourRange(float minr, float maxr, float ming, float maxg, float minb, float maxb, Interval a){
+		this(
+				minr == maxr ? new FixedValue(minr) : new Interval(minr,maxr),
+				ming == maxg ? new FixedValue(ming) : new Interval(ming,maxg),
+				minb == maxb ? new FixedValue(minb) : new Interval(minb,maxb),
+				a
+			);
 	}
 	
+	/**
+	 * Creates a new ColourRange object of specified ranges for red, green, blue and alpha components respectfully in the form of minimum, then maximum parameters.
+	 */
+	public ColourRange(float minr, float maxr, float ming, float maxg, float minb, float maxb, float mina, float maxa) {
+		this(minr, maxr, ming, maxg, minb, maxb, mina == maxa ? new FixedValue(mina) : new Interval(mina,maxa));
+	}
+	
+	/**
+	 * Creates a new ColourRange object of specified ranges for red, green, blue and components respectfully in the form of minimum, then maximum parameters.<br />
+	 * The alpha channel will be set to a default fixed value of 1f (opaque).
+	 */
 	public ColourRange(float minr, float maxr, float ming, float maxg, float minb, float maxb) {
-		this(new Interval(minr,maxr),new Interval(ming,maxg),new Interval(minb,maxb),new FixedValue(1f));
+		this(minr,maxr,ming,maxg,minb,maxb,new FixedValue(1f));
 	}
 
 	@Override
--- src/utils/interval/one/Range.java
+++ src/utils/interval/one/Range.java
@@ -2,15 +2,18 @@ package utils.interval.one;
 
 import java.util.Random;
 
+/**
+ * An abstract class for defining ranges of one-dimensional objects (floats, doubles, colours etc...)
+ * @param T The class being spanned by the range.
+ */
 public abstract class Range<T> {
 	
 	protected static final Random rand = new Random();
-
+	
 	public abstract T random();
 	
 	public abstract T length();
 	
 	public abstract T getMin();
 	public abstract T getMax();
-	
 }
