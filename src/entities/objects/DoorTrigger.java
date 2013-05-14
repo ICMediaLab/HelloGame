28c1fea2cbb0eadf56c46fa3fed6cd258043c481
George Steed <gds12@imperial.ac.uk>
2013-05-14 22:45:29 +0100

    Pretty trigger fade :)

--- src/entities/objects/DoorTrigger.java
+++ src/entities/objects/DoorTrigger.java
@@ -8,6 +8,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.newdawn.slick.Animation;
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
@@ -28,6 +29,7 @@ public class DoorTrigger extends StaticRectEntity implements TriggerSource {
 	private final Set<TriggerEffect> triggerables = new HashSet<TriggerEffect>();
 	
 	private transient final Animation s;
+	private float filter = 1f;
 	
 	public DoorTrigger(int x, int y){
 		super(x,y,1,1);
@@ -47,7 +49,7 @@ public class DoorTrigger extends StaticRectEntity implements TriggerSource {
 	private Animation getTriggerAnimation(){
 		BufferedImage i = new BufferedImage(Config.getTileSize(), Config.getTileSize(), BufferedImage.TYPE_INT_ARGB);
 		java.awt.Graphics g = i.createGraphics();
-		g.setColor(java.awt.Color.RED);
+		g.setColor(java.awt.Color.WHITE);
 		g.fillRect(0, 0, Config.getTileSize(), Config.getTileSize());
 		Texture t = null;
 		try {
@@ -58,14 +60,19 @@ public class DoorTrigger extends StaticRectEntity implements TriggerSource {
 		return new Animation(new Image[]{ new Image(t) }, 1);
 	}
 	
-	public void update(GameContainer gc) { }
+	public void update(GameContainer gc) {
+		if(filter < 1f){
+			filter = Math.max(0f, filter - 0.02f);
+		}
+	}
 	
 	@Override
 	public void addTriggerEffect(TriggerEffect t) {
 		triggerables.add(t);
 	}
 	
-	public void triggered() {
+	protected void triggered() {
+		filter -= 0.02f;
 		for(TriggerEffect t : triggerables){
 			t.triggeredSource(this);
 		}
@@ -73,7 +80,9 @@ public class DoorTrigger extends StaticRectEntity implements TriggerSource {
 	
 	@Override
 	public void render(GameContainer gc, Graphics g) {
-		s.draw((getX()-1)*Config.getTileSize(), (getY()-1)*Config.getTileSize());
+		float filsq = filter*filter;
+		filsq *= filsq;
+		s.draw((getX()-1)*Config.getTileSize(), (getY()-1)*Config.getTileSize(), new Color(filsq, 1f - filsq, 0f, filter));
 	}
 	
 	@Override
